f06300207ad7a4c0ad74d232d18396707dfbb679
TheEclipsE <TheEclipsE@tut.by>
2024-08-18 19:22:58 +0200

    GetHashCode

---
+++ src/Play.With.GetHashCode/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Play.With.GetHashCode
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(new KeyValuePair<int, int>(1, 1).GetHashCode());//always the same as value type
+
+            Console.WriteLine("1234".GetHashCode());//always different as ref type - based on location in memory
+        }
+    }
+}
